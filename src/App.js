25261a1d5b98635b1ec68de35db1190cc73a234d
MeniDev <menisaran15@gmail.com>
2022-02-15 10:52:20 +0530

    grid api call done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 
 import './App.css';
+import React, { Component } from "react";
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Customer Details</h1><hr /><br />
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      customer: [],
+      name: ""
+    };
+  }
+
+  componentDidMount() {
+    var self = this;
+    axios.get("https://pos-api-react.azurewebsites.net/api/customer/all")
+      .then(function (response) {
+        console.log(JSON.stringify(response.data.data));
+        self.setState({ customer: response.data.data });
+      })
+      .catch(function (error) {
+        console.log(error)
+      });
+  }
+
+  // getCustomerDetails = () => {
+  //   axios.get("https://pos-api-react.azurewebsites.net/api/customer/all")
+  //     .then(function (response) {
+  //       console.log(JSON.stringify(response.data.data));
+
+  //       this.setState({ customer: response.data.data });
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error)
+  //     });
+  // }
+
+
+  render() {
+    let customerData = this.state.customer.map((item) => {
+      return (<tr key={item.customerCode}>
+        <td>{item.customerCode}</td>
+        <td>{item.customerName}</td>
+        <td>{item.mobileNumber}</td>
+        <td className="text-center "><a href='#'>Edit</a> | <a href='#'>Delete</a></td>
+      </tr>);
+    });
+
+
+    return (<div className="container">
+      <h2>{this.state.name}</h2>
+      <h1 className='text-center'>Customer Details</h1><hr /><br />
       <div className='row' style={{ paddingBottom: 20 }}>
         <div className='col-md-12 text-right'>
           <a href="/add" className="btn btn-outline-success rightAlign">+ Add Customer</a>
@@ -15,7 +62,7 @@ function App() {
       <table className="table table-bordered" style={{ paddingTop: 10 }}>
         <thead className="thead-light" >
 
-          <tr>
+          <tr className='text-center'>
             <th >Customer Code</th>
             <th >Customer Name</th>
             <th >Mobile Number</th>
@@ -23,29 +70,12 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>001</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td className="text-center"><a href='#'>Edit</a> | <a href='#'>Delete</a></td>
-          </tr>
-          <tr>
-            <td>002</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td className="text-center"><a href='#'>Edit</a> | <a href='#'>Delete</a></td>
-          </tr>
-          <tr>
-            <td>003</td>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td className="text-center "><a href='#'>Edit</a> | <a href='#'>Delete</a></td>
-          </tr>
+          {customerData}
         </tbody>
       </table>
 
-    </div >
-  );
+    </div >)
+  }
 }
 
 export default App;
